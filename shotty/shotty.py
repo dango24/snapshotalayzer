710379b6b19e3e0caf8f89ab7c1c5bdae077e1cb
dangolan <dangolan@amazon.com>
2018-11-08 18:08:33 +0200

    Dangoooooo

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -8,12 +8,14 @@ filters = [{"Name": "tag:Project", "Values":["learn_python"]}]
 @click.command()
 def list_instances():
     for i in ec2.instances.filter(Filters=filters):
+        tags = {t['Key']: t['Value'] for t in i.tags or []}
         print(", ".join((
             i.id,
             i.instance_type,
             i.placement["AvailabilityZone"],
             i.state['Name'],
-            i.public_dns_name
+            i.public_dns_name,
+            tags.get("Project", "<No projects>")
         )))
 
 if __name__ == '__main__':
